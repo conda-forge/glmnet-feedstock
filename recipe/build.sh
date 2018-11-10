d3bacb4419830febc0cb19269bbe725d32cada1a
Matthew R Becker <becker.mr@gmail.com>
2018-11-10 13:38:13 -0600

    try this

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # see https://github.com/numpy/numpy/issues/7427
-LDFLAGS="$LDFLAGS -undefined dynamic_lookup -bundle"
+# LDFLAGS="$LDFLAGS -undefined dynamic_lookup -bundle"
+export NPY_DISTUTILS_APPEND_FLAGS=1
 $PYTHON -m pip install . --no-deps -vvv
